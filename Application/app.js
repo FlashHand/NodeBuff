4f1ac2a24267d04d506a349216ca477d01acaee1
Ral Wayne <ralwayne@163.com>
2016-09-30 10:42:20 +0800

    nm

--- Application/app.js
+++ Application/app.js
@@ -1,15 +1,16 @@
 const HOST = '127.0.0.1';
 const PORT1 = 5050;//数据库端口
 const PORT2 = 5000;//实时数据端口
+const TIMEOUT_LONG = 60;
 var net = require('net');
   queryServer=net.createServer(() => {}).listen(PORT1);
   queryServer.on('connection',(client) => {
     //与客户端建立连接
     console.log('connection:' + client.remoteAddress);
-    client.setTimeout(TIMEOUT_LONG,() => {
-      console.log('destroy' + client.remoteAddress);
-      client.destroy();
-    });
+    // client.setTimeout(TIMEOUT_LONG,() => {
+    //   console.log('destroy' + client.remoteAddress);
+    //   client.destroy();
+    // });
     //收到客户端消息
     client.on('data', function(receivedData) {
       console.log(receivedData);
