23225cd29e210fe50c88ed313e5fafdf1826f155
Ral Wayne <ralwayne@163.com>
2016-09-30 11:03:36 +0800

    nm

--- Application/app.js
+++ Application/app.js
@@ -6,46 +6,46 @@ var net = require('net');
 queryServer=net.createServer(() => {}).listen(PORT1);
 queryServer.on('connection',(client) => {
   //与客户端建立连接
-  console.log(PORT1+'connection:' + client.remoteAddress);
+  console.log(PORT1 + '~' +'connection:' + client.remoteAddress);
   // client.setTimeout(TIMEOUT_LONG,() => {
   //   console.log('destroy' + client.remoteAddress);
   //   client.destroy();
   // });
   //收到客户端消息
   client.on('data', function(receivedData) {
-    console.log(PORT1+receivedData);
+    console.log(PORT1 + '~' +receivedData);
   });
 
   //和客户端断开
   client.on('close', function(data) {
-      console.log(PORT1+'CLOSED: ' +
+      console.log(PORT1 + '~' +'CLOSED: ' +
           client.remoteAddress + ' ' + client.remotePort);
   });
   client.on('error', function(error) {
-      console.log(PORT1+'error: ' + error);
+      console.log(PORT1 + '~' +'error: ' + error);
   });
 
 });
 upServer=net.createServer(() => {}).listen(PORT2);
 upServer.on('connection',(client) => {
   //与客户端建立连接
-  console.log(PORT2+'connection:' + client.remoteAddress);
+  console.log(PORT2 + '~' +'connection:' + client.remoteAddress);
   // client.setTimeout(TIMEOUT_LONG,() => {
   //   console.log('destroy' + client.remoteAddress);
   //   client.destroy();
   // });
   //收到客户端消息
   client.on('data', function(receivedData) {
-    console.log(PORT2+receivedData);
+    console.log(PORT2 + '~' +receivedData);
   });
 
   //和客户端断开
   client.on('close', function(data) {
-      console.log(PORT2+'CLOSED: ' +
+      console.log(PORT2 + '~' +'CLOSED: ' +
           client.remoteAddress + ' ' + client.remotePort);
   });
   client.on('error', function(error) {
-      console.log(PORT2+'error: ' + error);
+      console.log(PORT2 + '~' +'error: ' + error);
   });
 
 });
